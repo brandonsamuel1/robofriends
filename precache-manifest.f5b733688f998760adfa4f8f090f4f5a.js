75ce444810b77a45bf2f9f3c5bcba6b5cece60ab
Brandon <brandonsamuel72@gmail.com>
2019-10-20 00:38:23 -0700

    Updates

---
+++ precache-manifest.f5b733688f998760adfa4f8f090f4f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ccb1b06dfb3dbe80bf32244b019963",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "07e86aa12f6794c51d81",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b63b6ea873bff28016c6",
+    "url": "/robofriends/static/css/main.063c80c7.chunk.css"
+  },
+  {
+    "revision": "07e86aa12f6794c51d81",
+    "url": "/robofriends/static/js/2.e6eb871a.chunk.js"
+  },
+  {
+    "revision": "b63b6ea873bff28016c6",
+    "url": "/robofriends/static/js/main.20972d0b.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
